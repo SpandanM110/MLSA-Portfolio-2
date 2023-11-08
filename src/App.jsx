83a8994f9c3e005521700d4e1329992e6d4fc62b
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-11-08 05:48:37 +0000

    data changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Spandan Mukherjee",
+  title: "Web Developer",
+  email: "spandanmukherjeegithub@gmail.com",
+  gitHub: "SpandanM110",
+  instagram: "slit_of_sunshine",
+  linkedIn: "spandanm110",
+  medium: "spandanmukherjee2003",
+  twitter: "spandan_twts",
+  youTube: "spandanmukherjee51",
 };
 
 const primaryColor = "#4E567E";
